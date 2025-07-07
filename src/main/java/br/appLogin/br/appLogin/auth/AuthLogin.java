8440ec2e89cd7ada614c2d6d8233ca4ce057b571
GabrielDemesio <cfmoneygamer@gmail.com>
2025-07-07 09:45:15 -0300

    melhorias no projeto

--- src/main/java/br/appLogin/br/appLogin/auth/AuthLogin.java
+++
@@ -1,4 +0,0 @@
-package br.appLogin.br.appLogin.auth;
-
-public class AuthLogin {
-}
--- src/main/java/br/appLogin/br/appLogin/controllers/LoginController.java
+++ src/main/java/br/appLogin/br/appLogin/controllers/LoginController.java
@@ -2,8 +2,10 @@ package br.appLogin.br.appLogin.controllers;
 
 import br.appLogin.br.appLogin.models.UserModel;
 import br.appLogin.br.appLogin.repositories.UserRepository;
+import br.appLogin.br.appLogin.service.CookieService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -14,20 +16,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.UnsupportedEncodingException;
+
 @Controller
 public class LoginController {
 
     private UserRepository userRepository;
 
     @GetMapping("/login")
-    public String login() {
+    public String login(HttpServletRequest request) {
         return "login";
     }
 
     @PostMapping("/logar")
-    public String logar(@Valid UserModel user, HttpServletResponse response) {
+    public String logar(@Valid UserModel user, HttpServletResponse response) throws UnsupportedEncodingException {
         UserModel userModel = this.userRepository.login(user.getEmail(), user.getSenha());
         if (userModel != null) {
+            CookieService.setCookie(response, "userId", String.valueOf(userModel.getId()), 10000);
+            CookieService.setCookie(response, "nameUser", String.valueOf(userModel.getNome()), 10000);
             return "redirect:/";
         }
         return "login/login";
